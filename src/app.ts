13ad7f41c0067371423b8a64db23407d1bb24528
Luiz Rioja <lulyrioja@gmail.com>
2023-07-16 16:01:24 -0300

    chore(app): register fastify jwt and cookie

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,24 @@
 import fastify from 'fastify'
 import { ZodError } from 'zod'
+import fastifyJwt from '@fastify/jwt'
+import fastifyCookie from '@fastify/cookie'
 
 import { env } from './env'
 
 export const app = fastify()
+
+app.register(fastifyJwt, {
+  secret: env.JWT_SECRET,
+  cookie: {
+    cookieName: 'refreshToken',
+    signed: false,
+  },
+  sign: {
+    expiresIn: '10m',
+  },
+})
+
+app.register(fastifyCookie)
 
 app.setErrorHandler((error, _, reply) => {
   if (error instanceof ZodError) {
