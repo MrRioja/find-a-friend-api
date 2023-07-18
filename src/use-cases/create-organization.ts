fb2635fab053a3e74f4d4c51063c1dc0a118ed69
Luiz Rioja <lulyrioja@gmail.com>
2023-07-18 18:26:30 -0300

    feat(useCases): implement create org

---
+++ src/use-cases/create-organization.ts
@@ -0,0 +1,47 @@
+import { Organization } from '@prisma/client'
+
+import { UserAlreadyExistsError } from './errors/user-already-exists-error'
+import { OrganizationsRepository } from '@/repositories/organizations-repository'
+
+interface CreateOrganizationUseCaseRequest {
+  cep: string
+  email: string
+  address: string
+  whatsapp: string
+  password: string
+  responsible_name: string
+}
+
+interface CreateOrganizationUseCaseResponse {
+  organization: Organization
+}
+
+export class CreateOrganizationUseCase {
+  constructor(private organizationsRepository: OrganizationsRepository) {}
+
+  async execute({
+    cep,
+    email,
+    address,
+    password,
+    whatsapp,
+    responsible_name,
+  }: CreateOrganizationUseCaseRequest): Promise<CreateOrganizationUseCaseResponse> {
+    const org = await this.organizationsRepository.findByEmail(email)
+
+    if (org) {
+      throw new UserAlreadyExistsError()
+    }
+
+    const organization = await this.organizationsRepository.create({
+      cep,
+      email,
+      address,
+      password,
+      whatsapp,
+      responsible_name,
+    })
+
+    return { organization }
+  }
+}
