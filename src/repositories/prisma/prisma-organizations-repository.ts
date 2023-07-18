7d96d5e86c79e22c4e35285f942f4adcf9f147ac
Luiz Rioja <lulyrioja@gmail.com>
2023-07-18 18:21:10 -0300

    feat(repositories): implement prisma organizations repository

---
+++ src/repositories/prisma/prisma-organizations-repository.ts
@@ -0,0 +1,22 @@
+import { Prisma } from '@prisma/client'
+
+import { prisma } from '@/lib/prisma'
+import { OrganizationsRepository } from '../organizations-repository'
+
+export class PrismaOrganizationsRepository implements OrganizationsRepository {
+  async create(data: Prisma.OrganizationCreateInput) {
+    const organization = await prisma.organization.create({ data })
+
+    return organization
+  }
+
+  async findByEmail(email: string) {
+    const organization = await prisma.organization.findUnique({
+      where: {
+        email,
+      },
+    })
+
+    return organization
+  }
+}
