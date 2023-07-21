1798a13a5932d26185d54595d4ebf5258d886ad7
Luiz Rioja <lulyrioja@gmail.com>
2023-07-20 23:52:32 -0300

    test(createOrg): validate throw error if orgs email already exists

--- src/use-cases/create-organization.spec.ts
+++ src/use-cases/create-organization.spec.ts
@@ -1,6 +1,7 @@
 import { describe, it, expect, beforeEach } from 'vitest'
 
 import { CreateOrganizationUseCase } from './create-organization'
+import { UserAlreadyExistsError } from './errors/user-already-exists-error'
 import { InMemoryOrganizationsRepository } from '@/repositories/in-memory/in-memory-organizations-repository'
 
 let sut: CreateOrganizationUseCase
@@ -23,5 +24,29 @@ describe('Create Organization Use Case', () => {
     })
 
     expect(organization.id).toEqual(expect.any(String))
+  })
+
+  it('should not be able to create two orgs with the same email', async () => {
+    const email = 'org@email.com'
+
+    await sut.execute({
+      email,
+      cep: '04848-515',
+      password: '123456',
+      whatsapp: '81 91234.5678',
+      address: 'Avenida Paulista',
+      responsible_name: 'Org Responsible',
+    })
+
+    await expect(() =>
+      sut.execute({
+        email,
+        cep: '04848-515',
+        password: '123456',
+        whatsapp: '81 91234.5678',
+        address: 'Avenida Paulista',
+        responsible_name: 'Org Responsible',
+      }),
+    ).rejects.toBeInstanceOf(UserAlreadyExistsError)
   })
 })
