c7ae97ed367d91c56d852c710dad03745108a40c
Luiz Rioja <lulyrioja@gmail.com>
2023-07-30 13:32:12 -0300

    feat(petsRepository): implement prisma repository

---
+++ src/repositories/prisma/prisma-pets-repository.ts
@@ -0,0 +1,14 @@
+import { Prisma } from '@prisma/client'
+
+import { prisma } from '@/lib/prisma'
+import { PetsRepository } from '../pets-repository'
+
+export class PrismaPetsRepository implements PetsRepository {
+  async create(data: Prisma.PetUncheckedCreateInput) {
+    const pet = await prisma.pet.create({
+      data,
+    })
+
+    return pet
+  }
+}
