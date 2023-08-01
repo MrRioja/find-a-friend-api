a7d2b1b29b82e81dc02234af381787f2fe9cf9db
Luiz Rioja <lulyrioja@gmail.com>
2023-08-01 20:53:30 -0300

    test(pets): add e2e test for create controller

---
+++ src/http/controllers/pets/create.spec.ts
@@ -0,0 +1,34 @@
+import { app } from '@/app'
+import request from 'supertest'
+import { afterAll, beforeAll, describe, expect, it } from 'vitest'
+
+import { createAndAuthenticateOrganization } from '@/utils/test/create-and-authenticate-organization'
+
+describe('Create Pet (e2e)', () => {
+  beforeAll(async () => {
+    await app.ready()
+  })
+
+  afterAll(async () => {
+    await app.close()
+  })
+
+  it('should be able to create a pet', async () => {
+    const { token } = await createAndAuthenticateOrganization(app)
+
+    const response = await request(app.server)
+      .post('/pets')
+      .set('Authorization', `Bearer ${token}`)
+      .send({
+        age: 1,
+        name: 'Dog',
+        size: 'small',
+        about: 'A small dog',
+        energy_level: 'high',
+        independence_level: 'low',
+        environment: 'small environments',
+      })
+
+    expect(response.statusCode).toEqual(201)
+  })
+})
