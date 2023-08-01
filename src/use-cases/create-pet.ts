c06dffe187d60c794234efe37dd7c557017acb55
Luiz Rioja <lulyrioja@gmail.com>
2023-08-01 20:47:27 -0300

    feat(pets): implement create use case

---
+++ src/use-cases/create-pet.ts
@@ -0,0 +1,46 @@
+import { Pet } from '@prisma/client'
+
+import { PetsRepository } from '@/repositories/pets-repository'
+
+interface CreatePetUseCaseRequest {
+  age: number
+  name: string
+  size: string
+  about: string
+  environment: string
+  energy_level: string
+  organization_id: string
+  independence_level: string
+}
+
+interface CreatePetUseCaseResponse {
+  pet: Pet
+}
+
+export class CreatePetUseCase {
+  constructor(private petsRepository: PetsRepository) {}
+
+  async execute({
+    age,
+    name,
+    size,
+    about,
+    environment,
+    energy_level,
+    organization_id,
+    independence_level,
+  }: CreatePetUseCaseRequest): Promise<CreatePetUseCaseResponse> {
+    const pet = await this.petsRepository.create({
+      name,
+      size,
+      about,
+      environment,
+      energy_level,
+      organization_id,
+      age: Number(age),
+      independence_level,
+    })
+
+    return { pet }
+  }
+}
