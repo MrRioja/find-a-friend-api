4ae9071b39a2a28922e0bff88dfb50792a1c4f29
Luiz Rioja <lulyrioja@gmail.com>
2023-08-02 15:00:00 -0300

    feat(repositories): add list method in pet repository

--- src/repositories/pets-repository.ts
+++ src/repositories/pets-repository.ts
@@ -1,5 +1,15 @@
 import { Prisma, Pet } from '@prisma/client'
 
+export interface ListData {
+  city: string
+  age?: number
+  size?: string
+  environment?: string
+  energy_level?: string
+  independence_level?: string
+}
+
 export interface PetsRepository {
   create(data: Prisma.PetUncheckedCreateInput): Promise<Pet>
+  list(data: ListData): Promise<Pet[]>
 }
