f031a6cb35906a924fd1fd3e95ae5aee13298b52
Luiz Rioja <lulyrioja@gmail.com>
2023-08-05 14:50:08 -0300

    feat(repositories): create pet in-memory repository

---
+++ src/repositories/in-memory/in-memory-pets-repository.ts
@@ -0,0 +1,53 @@
+import { randomUUID } from 'crypto'
+import { Prisma, Pet } from '@prisma/client'
+
+import { ListData, PetsRepository } from '../pets-repository'
+
+export class InMemoryPetsRepository implements PetsRepository {
+  public items: Pet[] = []
+
+  async create(data: Prisma.PetUncheckedCreateInput) {
+    const pet: Pet = {
+      age: data.age,
+      city: data.city,
+      name: data.name,
+      size: data.size,
+      id: randomUUID(),
+      about: data.about,
+      created_at: new Date(),
+      environment: data.environment,
+      energy_level: data.energy_level,
+      organization_id: data.organization_id,
+      independence_level: data.independence_level,
+    }
+
+    this.items.push(pet)
+
+    return pet
+  }
+
+  async list(data: ListData) {
+    const pets = this.items.filter((item) => {
+      if (item.city !== data.city) {
+        return false
+      }
+
+      if (
+        (data.age !== undefined && item.age !== data.age) ||
+        (data.size !== undefined && item.size !== data.size) ||
+        (data.environment !== undefined &&
+          item.environment !== data.environment) ||
+        (data.energy_level !== undefined &&
+          item.energy_level !== data.energy_level) ||
+        (data.independence_level !== undefined &&
+          item.independence_level !== data.independence_level)
+      ) {
+        return false
+      }
+
+      return true
+    })
+
+    return pets
+  }
+}
