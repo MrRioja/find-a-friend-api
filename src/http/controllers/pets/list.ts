76cd8c477e123c37955d2cbda9cd9d3d7486be76
Luiz Rioja <lulyrioja@gmail.com>
2023-08-05 12:28:19 -0300

    feat(controllers): implement list pets

---
+++ src/http/controllers/pets/list.ts
@@ -0,0 +1,30 @@
+import z from 'zod'
+import { FastifyReply, FastifyRequest } from 'fastify'
+import { makeListPetsUseCase } from '@/use-cases/factories/make-list-pets-use-case'
+
+export async function list(req: FastifyRequest, reply: FastifyReply) {
+  const listPetsQueryParamsSchema = z.object({
+    city: z.string().nonempty(),
+    size: z.string().optional(),
+    age: z.coerce.number().optional(),
+    environment: z.string().optional(),
+    energy_level: z.string().optional(),
+    independence_level: z.string().optional(),
+  })
+
+  const { city, age, energy_level, environment, independence_level, size } =
+    listPetsQueryParamsSchema.parse(req.query)
+
+  const listPetUseCase = makeListPetsUseCase()
+
+  const { pets } = await listPetUseCase.execute({
+    age,
+    city,
+    size,
+    environment,
+    energy_level,
+    independence_level,
+  })
+
+  return reply.status(201).send(pets)
+}
