4539c7a2ac55a1a23aceccc6196b0bf2319846bb
Luiz Rioja <lulyrioja@gmail.com>
2023-08-05 12:28:43 -0300

    feat(pets): add list route in the router

--- src/http/controllers/pets/routes.ts
+++ src/http/controllers/pets/routes.ts
@@ -1,5 +1,6 @@
 import { FastifyInstance } from 'fastify'
 
+import { list } from './list'
 import { create } from './create'
 import { verifyJWT } from '@/http/middlewares/verify-jwt'
 
@@ -7,4 +8,5 @@ export async function petsRoutes(app: FastifyInstance) {
   app.addHook('onRequest', verifyJWT)
 
   app.post('/pets', create)
+  app.get('/pets', list)
 }
